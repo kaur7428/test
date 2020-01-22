e6feb22a3291d563516673b870389d7c460e1cd4
kaur7428 <kaur7428@sheridancollege.ca>
2020-01-22 09:52:18 -0500

    4th commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
-    private String status;
+    private String statusa;
 
     public void setStatus(String status) {
         this.status = status;
